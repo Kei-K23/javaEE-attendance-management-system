8f445af686929b17c273eab370010bab09274117
Kei-K23 <keiksl2301@gmail.com>
2024-05-27 21:20:36 +0630

    Add login UI design

---
+++ src/main/java/dev/kei/controller/LoginServlet.java
@@ -0,0 +1,43 @@
+package dev.kei.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+//	@Override
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//			throws IOException, ServletException {
+//
+//		UserService userService = new UserService();
+//
+//		String username = request.getParameter("username");
+//		String password = request.getParameter("password");
+//
+//		User dbUser = userService.findByUsername(username);
+//
+//		if (username.equals(dbUser.getUsername()) && PasswordUtil.verifyPassword(password, dbUser.getPassword())) {
+//			HttpSession session = request.getSession();
+//			session.setAttribute("username", username);
+//			session.setMaxInactiveInterval(60 * 60 * 24);
+//			response.sendRedirect("account.jsp");
+//		} else {
+//			request.setAttribute("errorMsg", "Invalid username or password");
+//			getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+//		}
+//	}
+}
